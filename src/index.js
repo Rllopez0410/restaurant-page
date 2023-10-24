9778a47fc3b89055bdbf92d7133c7ebd67ec1a56
Roger Lopez <rllopez0410@gmail.com>
2023-10-24 02:57:14 -0400

    reset

--- src/index.js
+++ src/index.js
@@ -1,2 +1,20 @@
-import { scss } from "./index.scss";
-import { mainPage } from "./main-page.js";
+const content = document.getElementById("content");
+
+ // -- Navigation Bar --
+
+const navBar = document.createElement("div");
+const homeTab = document.createElement("div");
+const menuTab = document.createElement("div");
+const contactTab = document.createElement("div");
+
+content.appendChild(navBar);
+navBar.appendChild(homeTab);
+navBar.appendChild(menuTab);
+navBar.appendChild(contactTab);
+
+homeTab.textContent = "Home";
+menuTab.textContent = "Menu";
+contactTab.textContent = "Contact";
+
+// -- Main --
+
